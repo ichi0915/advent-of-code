6220a078537bcf6ebd749e3ea7602ab0a576010c
ichi0915 <ichi0915@gmail.com>
2022-12-05 21:49:12 -0600

    feat(2022-fourth-puzzle): Find the sum of assignment pairs that overlap

--- 2022/day-4/main.go
+++ 2022/day-4/main.go
@@ -52,6 +52,20 @@ In how many assignment pairs does one range fully contain the other?
 
 --- Part Two ---
 
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+    5-7,7-9 overlaps in a single section, 7.
+    2-8,3-7 overlaps all of the sections 3 through 7.
+    6-6,4-6 overlaps in a single section, 6.
+    2-6,4-8 overlaps in sections 4, 5, and 6.
+
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
+
+
 -- Data ichi --
 
 */
@@ -73,6 +87,9 @@ func main() {
 
 	sum := getSumOfAssignmentPairsThatFullyContainTheOtherWrap(cleaningSection)
 	fmt.Println("Sum of the contains: ", sum)
+
+	sum = getSumOfAssignmentPairsThatOverlapTheOtherWrap(cleaningSection)
+	fmt.Println("Sum of the overlaps: ", sum)
 
 	/*cleaningSectionValues := unwrapCleaningSections(cleaningSection)
 	// print2DArray(cleaningSectionValues)
@@ -124,6 +141,27 @@ func getSumOfAssignmentPairsThatFullyContainTheOtherWrap(arr [][]string) int {
 		if leftBottom <= rightBottom && leftTop >= rightTop {
 			sum++
 		} else if rightBottom <= leftBottom && rightTop >= leftTop {
+			sum++
+		}
+	}
+
+	return sum
+}
+
+func getSumOfAssignmentPairsThatOverlapTheOtherWrap(arr [][]string) int {
+	sum := 0
+
+	for i := range arr {
+		leftBottom, _ := strconv.Atoi(strings.Split(arr[i][0], "-")[0])
+		leftTop, _ := strconv.Atoi(strings.Split(arr[i][0], "-")[1])
+		rightBottom, _ := strconv.Atoi(strings.Split(arr[i][1], "-")[0])
+		rightTop, _ := strconv.Atoi(strings.Split(arr[i][1], "-")[1])
+
+		if leftBottom <= rightBottom && leftTop >= rightTop {
+			sum++
+		} else if rightBottom <= leftBottom && rightTop >= leftTop {
+			sum++
+		} else if leftTop >= rightBottom && rightTop >= leftBottom {
 			sum++
 		}
 	}
