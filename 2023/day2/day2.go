c2f60e6fdf7b70a445f52c15be1a4fc6621f5c88
ichi0915 <ichi0915@gmail.com>
2023-12-03 22:31:53 -0600

    feat(2023): Added day 2 part two

--- 2023/day2/day2.go
+++ 2023/day2/day2.go
@@ -35,6 +35,33 @@ game 4 would also have been impossible because the Elf showed you 15 blue cubes 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
 What is the sum of the IDs of those games?
 
+--- Part Two ---
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped;
+however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color
+that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes.
+If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
+The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively.
+Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 */
 
 package day2
@@ -88,6 +115,18 @@ func Main() {
 
 	posibleGamesSum := sumValues(posibleGames)
 	fmt.Println("posibleGamesSum: ", posibleGamesSum)
+
+	// --- Part Two ---
+
+	multiplicationList := multiplyValues(gamesList)
+	if debug {
+		fmt.Println("Printing multiplication List")
+		printArrayInt(multiplicationList)
+	}
+
+	minValuesSum := sumValues(multiplicationList)
+	fmt.Println("minValuesSum: ", minValuesSum)
+
 }
 
 func getPosibleGames(gamesList [][]int, colorsLoaded []int) []int {
@@ -180,6 +219,17 @@ func sumValues(posibleGames []int) int {
 
 	for _, val := range posibleGames {
 		response += val
+	}
+
+	return response
+}
+
+func multiplyValues(gamesList [][]int) []int {
+	var response []int
+
+	for _, val := range gamesList {
+		multiplication := val[0] * val[1] * val[2]
+		response = append(response, multiplication)
 	}
 
 	return response
