60eafa17e3f2ed8b45630d440f4c39db318efe9c
ichi0915 <ichi0915@gmail.com>
2023-12-04 22:02:33 -0600

    feat(2023): Added wrong part 2 of day 3

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -34,6 +34,40 @@ Every other number is adjacent to a symbol and so is a part number; their sum is
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
 
+--- Part Two ---
+The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola,
+finally ready to ascend to the water source.
+
+You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help",
+so you pick it up and the engineer answers.
+
+Before you can explain the situation, she suggests that you look out the window. There stands the engineer,
+holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+
+The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers.
+Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+In this schematic, there are two gears.
+The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345.
+The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.)
+Adding up all of the gear ratios produces 467835.
+
+What is the sum of all of the gear ratios in your engine schematic?
+
 */
 
 package day3
@@ -42,6 +76,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -75,7 +110,7 @@ var rowAmount int
 func Main() {
 	trace = false
 	debug = false
-	debug = true
+	// debug = true
 	fmt.Println("== Day 3 of 2023 ==")
 
 	readData := readFileData2D()
@@ -96,15 +131,25 @@ func Main() {
 		fmt.Println("\nRow Amount: ", rowAmount, "\n")
 	}
 
-	partNumbers := getAdjacentPartNumbers(numbers, symbols)
+	// --- Part One ---
+	// partNumbers := getAdjacentPartNumbers(numbers, symbols)
+	// if debug {
+	// 	fmt.Println("Printing Part Numbers")
+	// 	printArrayInt(partNumbers)
+	// }
+
+	// partNumbersSum := sumValues(partNumbers)
+	// fmt.Println("partNumbersSum: ", partNumbersSum)
+
+	// --- Part Two ---
+	gearRatios := getGearRatio(numbers, symbols)
 	if debug {
-		fmt.Println("Printing Part Numbers")
-		printArrayInt(partNumbers)
+		fmt.Println("Printing Gear Ratios")
+		printArrayInt(gearRatios)
 	}
 
-	partNumbersSum := sumValues(partNumbers)
+	partNumbersSum := sumValues(gearRatios)
 	fmt.Println("partNumbersSum: ", partNumbersSum)
-
 }
 
 func getNumbersAndSymbolsPos(readData [][]string) ([]Numbers, []Symbols) {
@@ -156,6 +201,71 @@ func getNumbersAndSymbolsPos(readData [][]string) ([]Numbers, []Symbols) {
 	}
 
 	return responseNumbers, responseSymbols
+}
+
+func getGearRatio(numbers []Numbers, symbols []Symbols) []int {
+	var response []int
+	var responseNumbers []int
+	var symbolId string
+	var adjacentMap = make(map[string][]int)
+
+	for _, symbolVal := range symbols {
+		if "*" == symbolVal.symbol {
+
+			responseNumbers = make([]int, 0)
+			gearCoordinates := getGearCoordinates(symbolVal)
+			if debug {
+				printSymbols(symbolVal)
+				printCoordinatesArray(gearCoordinates)
+			}
+
+			for _, numberVal := range numbers {
+
+				numberCoordinates := getExactNumberCoordinates(numberVal)
+				if debug {
+					printNumbers(numberVal)
+					printCoordinatesArray(numberCoordinates)
+				}
+
+				isAdjacent := checkIfAdjacentGear(numberCoordinates, gearCoordinates)
+				if isAdjacent {
+					if debug {
+						fmt.Println("isAdjacent", isAdjacent)
+					}
+					responseNumbers = append(responseNumbers, numberVal.value)
+				}
+			}
+			// symbolId = fmt.Sprint(symbolVal.xPos, "-", symbolVal.yPos, "-", RandomString(10))
+			symbolId = fmt.Sprint(symbolVal.xPos, "-", symbolVal.yPos)
+			adjacentMap[symbolId] = responseNumbers
+		}
+
+		// fmt.Println("value:", value, " yPos:", yPos, " xPosArr:", xPosArr)
+	}
+
+	for _, adjacentArray := range adjacentMap {
+		multiplication := 1
+
+		if 1 < len(adjacentArray) {
+			// fmt.Println("adjacentArray:", adjacentArray)
+			for _, val := range adjacentArray {
+				multiplication *= val
+			}
+			response = append(response, multiplication)
+		}
+	}
+
+	return response
+}
+
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }
 
 func getAdjacentPartNumbers(numbers []Numbers, symbols []Symbols) []int {
@@ -240,6 +350,70 @@ func getNumberCoordinates(number Numbers) []Coordinates {
 	return coordinates
 }
 
+// Return the Exact Number coordinates for the number
+func getExactNumberCoordinates(number Numbers) []Coordinates {
+	var coordinates []Coordinates
+	var coordinate Coordinates
+
+	for i := number.xStart; i < (number.xStart + number.len); i++ {
+		coordinate = Coordinates{yPos: number.yPos, xPos: i}
+		coordinates = append(coordinates, coordinate)
+	}
+
+	return coordinates
+}
+
+// Return all the posible adjacent coordinates for the gear
+func getGearCoordinates(symbol Symbols) []Coordinates {
+	var coordinates []Coordinates
+	var coordinate Coordinates
+
+	// Los valores de arriba
+	if 0 != symbol.yPos {
+		if 0 != symbol.xPos {
+			coordinate = Coordinates{yPos: symbol.yPos - 1, xPos: symbol.xPos - 1}
+			coordinates = append(coordinates, coordinate)
+		}
+
+		coordinate = Coordinates{yPos: symbol.yPos - 1, xPos: symbol.xPos}
+		coordinates = append(coordinates, coordinate)
+
+		if columnAmount >= (symbol.xPos + 1) {
+			coordinate = Coordinates{yPos: symbol.yPos - 1, xPos: symbol.xPos + 1}
+			coordinates = append(coordinates, coordinate)
+		}
+
+	}
+
+	// Los valores de en medio
+	if 0 < symbol.xPos {
+		coordinate = Coordinates{yPos: symbol.yPos, xPos: symbol.xPos - 1}
+		coordinates = append(coordinates, coordinate)
+	}
+	if columnAmount >= (symbol.xPos + 1) {
+		coordinate = Coordinates{yPos: symbol.yPos, xPos: symbol.xPos + 1}
+		coordinates = append(coordinates, coordinate)
+	}
+
+	// Los valores de abajo
+	if rowAmount > symbol.yPos {
+		if 0 != symbol.xPos {
+			coordinate = Coordinates{yPos: symbol.yPos + 1, xPos: symbol.xPos - 1}
+			coordinates = append(coordinates, coordinate)
+		}
+
+		coordinate = Coordinates{yPos: symbol.yPos + 1, xPos: symbol.xPos - 1}
+		coordinates = append(coordinates, coordinate)
+
+		if columnAmount >= (symbol.xPos + 1) {
+			coordinate = Coordinates{yPos: symbol.yPos + 1, xPos: symbol.xPos + 1}
+			coordinates = append(coordinates, coordinate)
+		}
+	}
+
+	return coordinates
+}
+
 func checkIfAdjacent(coordinates []Coordinates, symbol Symbols) bool {
 	response := false
 
@@ -248,6 +422,22 @@ func checkIfAdjacent(coordinates []Coordinates, symbol Symbols) bool {
 			coordinate.yPos == symbol.yPos {
 			response = true
 			break
+		}
+	}
+
+	return response
+}
+
+func checkIfAdjacentGear(numberCoordinates []Coordinates, gearCoordinates []Coordinates) bool {
+	response := false
+
+	for _, numberCoordinate := range numberCoordinates {
+		for _, gearCoordinate := range gearCoordinates {
+			if gearCoordinate.xPos == numberCoordinate.xPos &&
+				gearCoordinate.yPos == numberCoordinate.yPos {
+				response = true
+				break
+			}
 		}
 	}
 
@@ -296,11 +486,12 @@ func isInt(s string) bool {
 	return true
 }
 
-func sumValues(arr []int) int {
-	response := 0
+func sumValues(arr []int) uint64 {
+	var response uint64
+	response = 0
 
 	for _, val := range arr {
-		response += val
+		response += uint64(val)
 	}
 
 	return response
@@ -360,6 +551,7 @@ func print2DArrayFullV2(arr [][]string) {
 }
 
 func printNumbers(number Numbers) {
+	fmt.Println("== printNumbers ==")
 	fmt.Println(number.yPos, "-", number.xStart, ",", number.xFinish, " ", number.value, "  ", number.len)
 }
 
@@ -373,6 +565,7 @@ func printNumbersArray(numbers []Numbers) {
 }
 
 func printSymbols(symbol Symbols) {
+	fmt.Println("== printSymbols ==")
 	fmt.Println(symbol.yPos, "-", symbol.xPos, " ", symbol.symbol)
 }
 
